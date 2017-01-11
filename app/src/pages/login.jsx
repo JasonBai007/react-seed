5e5f2d74720088ab47bf2f707a1e0496fad47f5e
JasonBai007 <byygkcg@163.com>
2017-01-11 14:57:19 +0800

    升级登陆页面的form组件

--- app/src/pages/login.jsx
+++ app/src/pages/login.jsx
@@ -13,15 +13,20 @@ class LoginPage extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        let n = this.props.form.getFieldsValue().username;
-        let p = this.props.form.getFieldsValue().password;
-        if (n === 'ilovejasonbai' && p === 'ilovejasonbai') {
-            // 表单的路由处理    
-            document.cookie = "nowKey=" + "home";                   
-            browserHistory.push('/');
-        } else {
-            this.openNotificationWithIcon('info');
-        }
+        this.props.form.validateFields((err, values) => {
+          if (!err) {
+            console.log('Received values of form: ', values);
+            let n = values.username;
+            let p = values.password;
+            if (n === 'ilovejasonbai' && p === 'ilovejasonbai') {
+                // 表单的路由处理    
+                document.cookie = "nowKey=" + "home";                   
+                browserHistory.push('/');
+            } else {
+                this.openNotificationWithIcon('info');
+            }
+          }
+        });
     }
 
     // 返回一个弹框对象，提示用户名和密码
@@ -38,18 +43,26 @@ class LoginPage extends React.Component {
     }
 
     render() {  
-        const { getFieldProps } = this.props.form;      
+        const { getFieldDecorator } = this.props.form;      
         return (
             <div id="loginpagewrap">
                 <p>Sign in to BYY</p>
                 <div id="loginWrap">                
                     <Form horizontal onSubmit={this.handleSubmit}>
                         <FormItem>
-                            <Input placeholder="Username" {...getFieldProps('username')} />
+                            {getFieldDecorator('username', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
+                                <Input placeholder="Username" />
+                            )}
                         </FormItem>
                         <FormItem>
-                            <Input type="password" placeholder="Password" {...getFieldProps('password')} />
-                        </FormItem>                    
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                            })(
+                                <Input type="password" placeholder="Password" />
+                            )}
+                        </FormItem>                                           
                         <Button type="primary" htmlType="submit" id="loginBtn">Login</Button>
                     </Form>
                 </div>
