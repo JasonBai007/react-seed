f6c65398278e03cbb77ca91bc096789a84165781
JasonBai007 <byygkcg@163.com>
2017-01-12 10:04:00 +0800

    修复导航显示问题

--- app/src/components/Sidebar.jsx
+++ app/src/components/Sidebar.jsx
@@ -33,6 +33,7 @@ export default class Sidebar extends React.Component {
     }
     componentDidMount() {
         var now = window.location.pathname.substring(1);
+        now = now === '' ? 'home' : now;
         this.changeCookie(now);
         this.setState({ current: now });
     }
